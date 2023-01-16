54e9af5afe94f68beb068e742c491fd8a735e07a
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-01-16 23:22:48 +0100

    Navigation menu start
    Start of the navigation menu

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// Navigatie menu openen
+const menuBtn = document.getElementById('menu-btn');
+const topNav = document.getElementById('topnav');
+const closeBtn = document.getElementById('nav-close');
+
+menuBtn.addEventListener('click', function() {
+    topNav.classList.toggle('active-nav');
+    closeBtn.classList.toggle('show');
+    console.log('added class');
+});
+
+closeBtn.addEventListener('click', function() {
+    topNav.classList.remove('active-nav');
+    closeBtn.classList.remove('show');
+    console.log('removed class');
+});
